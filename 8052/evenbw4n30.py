9ae3b2713ef9fb04b8381b290b06ddd95f7fd107
Uday Bhatia <101352755+BhatiaUday@users.noreply.github.com>
2023-08-29 11:30:59 +0530

    Delete 8052 directory

--- 8052/evenbw4n30.py
+++
@@ -1,8 +0,0 @@
-x=4
-li=[]
-while x<=30:
-    if x%2==0:
-        li.append(x)
-    x+=1
-
-print(li)
--- 8052/keytodict.py
+++
@@ -1,6 +0,0 @@
-di={}
-while True:
-    key=input("Enter Key: ")
-    value=input("Enter value: ")
-    di[key]=value
-    print(di)
