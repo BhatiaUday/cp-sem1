28f4d5a2fb499a2cdcd5ae06144311300a26b7b3
Uday Bhatia <101352755+BhatiaUday@users.noreply.github.com>
2023-09-02 20:20:41 +0530

    updated matrice multiplication code

--- 29-aug-2023/matrice.py
+++ 29-aug-2023/matrice.py
@@ -1,13 +1,19 @@
-import numpy as np
+#https://github.com/BhatiaUday/cp-sem1/
+matrix1 = [[1,2,3], [4,5,6], [7,8,9]]
+matrix2 = [[9,8,7], [6,5,4], [3,2,1]]
 
-A=[[1,2,3],
-   [4,5,6],
-   [7,8,9]]
 
-B=[[1,2,3,4],
-   [4,5,6,7],
-   [6,7,8,9]]
-
-result=np.dot(A,B)
+rowsA = len(matrix1)
+colsA = len(matrix1[0])
+rowsB = len(matrix2)
+colsB = len(matrix2[0])
+if colsA != rowsB:
+    print("Cannot Multiply!")
+else:
+    result = [[0 for row in range(colsB)] for col in range(rowsA)]
+    for i in range(rowsA):
+        for j in range(colsB):
+            for k in range(colsA):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
 for r in result:
-   print(r)
+    print(r)
